d66cc1ad192b3d34404904896e5d61370d51df92
AdrianNiewiadomski <kontakt@adrianniewiadomski.pl>
2022-03-06 14:48:52 +0100

    The battery state notifier is running continuously.

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
+from time import sleep
+
 dir_path = "/sys/class/power_supply/BAT1"
 
 
-def get_parameter(parameter: str):
-    with open(dir_path + "/" + parameter, "r") as capacity_file:
-        capacity = capacity_file.read()
-    return capacity
+def get_parameter(parameter_name: str) -> str:
+    with open(dir_path + "/" + parameter_name, "r") as capacity_file:
+        parameter = capacity_file.read()
+    return parameter
+
+
+def run_state_notifier() -> None:
+    while True:
+        status = get_parameter("status").strip()
+        print(status)
+
+        if status.startswith("Discharging"):
+            print(get_parameter("capacity").strip())
+
+        sleep(30)
 
 
 if __name__ == '__main__':
-    print(get_parameter("status"))
-    print(get_parameter("capacity"))
+    run_state_notifier()
